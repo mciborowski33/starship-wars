7e9f88166ca9f5bf7239eee464ec6dd55da87ebb
mciborowski33 <mciborowski33@gmail.com>
2022-03-27 16:12:17 +0200

    extra cleanup

--- starship-wars-client/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { mount } from '@cypress/react';
-import App from './App';
-
-it('displays the title', () => {
-	mount(<App />);
-	cy.get('div').contains('Starships Wars');
-});
